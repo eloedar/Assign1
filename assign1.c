ea5b9d5dd60c4120a477aa705b8949f0e9eae39c
eloedar <jiaji5@live.com>
2022-03-06 00:21:18 +0800

    1

--- assign1.c
+++ assign1.c
@@ -4,21 +4,21 @@
 
 
 int main() {
-    printf("%d", quick_power(4, 3));
+    printf("%d", quick_power(258993530, 15223259));
     return 0;
 }
 
 int quick_power(int x, int n) {
-    int part_factor;
-    if (n == 0) {
-        return 1;
+    unsigned long long ans = 1;
+    unsigned long long power_factor = (unsigned long long) x;
+    while (n != 0) {
+        if (n % 2 != 0) {
+            ans = (ans * power_factor % (MODULO));
+        }
+        power_factor = (power_factor * power_factor%(MODULO));
+        n = n / 2;
     }
-    part_factor = quick_power(x, floor(n / 2));
-    if (n % 2 != 0) {
-        return (part_factor * part_factor * x) % (MODULO);
-    } else {
-        return (part_factor * part_factor) % (MODULO);
-    }
+    return ans % (MODULO);
 }
 
 int matrix_addition(matrix mat_a, matrix mat_b, matrix mat_res) {
@@ -45,8 +45,7 @@ int matrix_multiplication(matrix mat_a, matrix mat_b, matrix mat_res) {
     for (int i = 0; i < mat_res.m_row; ++i)
         for (int j = 0; j < mat_res.m_col; ++j) {
             int val = 0;
-            for (int k = 0; k < mat_a.m_col; ++k)
-            {
+            for (int k = 0; k < mat_a.m_col; ++k) {
                 val = val + ((long long) get_by_index(mat_a, i, k)) * get_by_index(mat_b, k, j) % MODULO;
             }
             set_by_index(mat_res, i, j, val);
